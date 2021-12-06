3810f9b4c6181e98b064b226ceb85527d57271c4
laksh patel <lakshy1662001@gmail.com>
2021-12-06 13:05:18 +0530

    xylophone

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: const Text("Cliked")
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
